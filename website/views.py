418c2a606415e089f86219c1c8d6a4f8fde3efcd
doubi3 <mark.ekperi@gmail.com>
2021-07-06 22:03:27 +0100

    db created and configured

--- website/views.py
+++ website/views.py
@@ -1,7 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def home():
-    return "<h1>Hello world<h1>"
+    return render_template("home.html", user=current_user)
+
+# @views.route('/login')
+# def login():
+#     return render_template("login.html")
+
+# @views.route('/register')
+# def register():
+#     return render_template("register.html")
+
